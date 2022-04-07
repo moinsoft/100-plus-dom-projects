cf6eb53e2e3da0424162256cce703907d16d9878
moinsoft <mmoin.official@gmail.com>
2022-04-07 14:48:36 +0600

    1st Project Initial Commit

---
+++ Project-1/app.js
@@ -0,0 +1,12 @@
+/**
+ * Project Requirements:
+ * - Change the background color by generating random rgb color by clicking a button.
+*/
+
+// Step 1 - Create onload handler.
+
+// step 2 - Random color generator function.
+
+// Step 3 - Collect all necessary references.
+
+// Step 4 - Handle the click event.
