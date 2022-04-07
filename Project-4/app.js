5666d337a73d8a10e5e13596dbb03384c36587d1
moinsoft <mmoin.official@gmail.com>
2022-04-07 23:10:09 +0600

    4th Project Done

--- Project-4/app.js
+++ Project-4/app.js
@@ -8,6 +8,11 @@
 */
 
 
+// Globals
+
+let div = null;
+
+
 // Step 1 - Create onload handler.
 window.onload = () => {
   main();
@@ -29,6 +34,13 @@ function main() {
 
   copyBtn.addEventListener('click', function () {
     navigator.clipboard.writeText(output.value);
+
+    if (div !== null) {
+      div.remove();
+      div = null;
+    }
+
+    generateToastMessage(`${output.value} copied`);
   })
 };
 
@@ -44,6 +56,26 @@ function generateHexColor() {
   const blue = Math.floor(Math.random() * 255);
 
   return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+}
+
+
+function generateToastMessage(msg) {
+  div = document.createElement('div');
+  div.innerText = msg;
+  div.className = 'toast-message toast-message-slide-in';
+
+  div.addEventListener('click', function () {
+    div.classList.remove('toast-message-slide-in');
+    div.classList.add('toast-message-slide-out');
+
+    div.addEventListener('animationend', function () {
+      div.remove();
+      div = null;
+    })
+
+  })
+
+  document.body.appendChild(div);
 }
 
 
@@ -66,3 +98,6 @@ function generateHexColor() {
 
 
 // Step 8 - Clear toast message.
+
+
+// Step 9 - Finally Done 4th Project.
